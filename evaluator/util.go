6e6883ce4b1c7a64f5e8463aca0a72f1aa2d67a2
Tikhon Belousov <58363010+it1shka@users.noreply.github.com>
2021-08-08 20:17:15 +0300

    commit

--- evaluator/util.go
+++ evaluator/util.go
@@ -35,6 +35,52 @@ func checkCondition(object OBJECT) bool {
 	}
 }
 
+func areNumbers(a, b OBJECT) bool {
+	return (a.getType() == INT_TYPE || a.getType() == FLOAT_TYPE) &&
+		(b.getType() == INT_TYPE || b.getType() == FLOAT_TYPE)
+}
+
+func ToFloat64(a OBJECT) (out float64) {
+	switch a_ := a.(type) {
+	case Float:
+		out = float64(a_)
+	case Int:
+		out = float64(a_)
+	}
+	return
+}
+
+type NumberOpFunc func(a, b float64) float64
+
+type NumberCompFunc func(a, b float64) bool
+
+func NumberOperation(a, b OBJECT, fn NumberOpFunc) OBJECT {
+	af, bf := ToFloat64(a), ToFloat64(b)
+	result := fn(af, bf)
+	if a.getType() == INT_TYPE && b.getType() == INT_TYPE {
+		return Int(result)
+	}
+	return Float(result)
+}
+
+func TryNumberOperation(a, b OBJECT, fn NumberOpFunc) (OBJECT, bool) {
+	if areNumbers(a, b) {
+		return NumberOperation(a, b, fn), true
+	}
+	return Null_, false
+}
+
+func TryNumberCompare(a, b OBJECT, fn NumberCompFunc) (OBJECT, bool) {
+	if areNumbers(a, b) {
+		result := fn(ToFloat64(a), ToFloat64(b))
+		return resolveBool(result), true
+	}
+	return Null_, false
+}
+
+// number functions...
+func addf(a, b float64) float64 { return a + b }
+
 func resolveBinaryOp(left, right OBJECT, operator string) OBJECT {
 
 	switch operator {
@@ -47,20 +93,8 @@ func resolveBinaryOp(left, right OBJECT, operator string) OBJECT {
 	case "!=":
 		return resolveBool(left != right)
 	case "+":
-		if left.getType() == INT_TYPE && right.getType() == INT_TYPE {
-			return Int(left.(Int) + right.(Int))
-		}
-
-		if left.getType() == FLOAT_TYPE && right.getType() == FLOAT_TYPE {
-			return Float(left.(Float) + right.(Float))
-		}
-
-		if left.getType() == FLOAT_TYPE && right.getType() == INT_TYPE {
-			return Float(left.(Float) + Float(right.(Int)))
-		}
-
-		if left.getType() == INT_TYPE && right.getType() == FLOAT_TYPE {
-			return Float(Float(left.(Int)) + right.(Float))
+		if obj, ok := TryNumberOperation(left, right, addf); ok {
+			return obj
 		}
 
 		if left.getType() == STRING_TYPE && right.getType() == STRING_TYPE {
